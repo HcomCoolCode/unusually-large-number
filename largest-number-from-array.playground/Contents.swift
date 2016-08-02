4a45d2c100613f898f8d7ac7a0c509276cdf7bf0
Maciej Matyjas <mmatyjas@expedia.com>
2016-08-02 10:48:47 +0100

    smallest possible commit number

---
+++ largest-number-from-array.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: 
+//: Original source for problem => https://www.shiftedup.com/2015/05/07/five-programming-problems-every-software-engineer-should-be-able-to-solve-in-less-than-1-hour
+
+import Cocoa
+import XCTest
+
+func largestNumberComposed(from: Array<Int>) -> Int {
+    return from[0]
+}
+
+/// tests
+
+XCTAssertEqual(9, largestNumberComposed(from: [9]))
+
+XCTAssertEqual(90, largestNumberComposed(from: [9,0]))
+
+// XCTAssertEqual(910, largestNumberComposed(from: [10,9]))
